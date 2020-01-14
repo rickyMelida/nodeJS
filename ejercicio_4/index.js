73f1f340fa5f03a465eefadea363849a6a63a303
Sub Estacion <subestacion@paseolagaleria.com>
2020-01-14 04:34:56 -0300

    Se agrega modulo de Express JS

--- ejercicio_4/index.js
+++ ejercicio_4/index.js
@@ -1,3 +1,4 @@
+/*
 //Llamamos al modulo http para crear servidores
 const http = require('http');
 
@@ -22,5 +23,24 @@ const server = http.createServer(handleServer);
 server.listen(3000, function() {
     console.log('Servidor en el puerto 3000');
     
+});*/
+
+
+//Express nos permite crear servidores
+const express = require('express');
+const colors = require('colors');
+
+const server = express();
+
+//cuando a mi servidor le pidan algo, vamos a responder con una funcion
+//La funcion recibe un parametro que es la peticion del cliente, y otra que es la respuesta del seridor
+server.get('/', function(req, res) {
+    //llamamos a un metodo de express 
+    res.send('<h1>Hola mundo con Express y Node JS</h1>');
+    res.end();
 });
 
+//Quiero que escuche por el puerto 3000 y una vez que la escuche que me envie un mensaje por consola a traves de una funcion
+server.listen(3000, function() {
+    console.log('Server on port 3000'.red);
+});
